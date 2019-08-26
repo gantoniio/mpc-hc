682518631403a02156c908a0ba9acb35a6484ea2
adipose <3324395+adipose@users.noreply.github.com>
2019-08-26 10:54:03 -0700

    .

--- src/mpc-hc/CMPCThemeScrollBar.h
+++ src/mpc-hc/CMPCThemeScrollBar.h
@@ -6,14 +6,14 @@ class CMPCThemeScrollBar :	public CXeScrollBarBase
 {
     DECLARE_DYNAMIC(CMPCThemeScrollBar)
 public:
-	CMPCThemeScrollBar();
-	virtual ~CMPCThemeScrollBar();
+    CMPCThemeScrollBar();
+    virtual ~CMPCThemeScrollBar();
     void DrawScrollBar(CDC * pDC);
     virtual void SendScrollMsg(WORD wSBcode, WORD wHiWPARAM);
     void setScrollWindow(CWnd* m_SBRepl) { this->m_scrollWindow = m_SBRepl; };
     void updateScrollInfo();
-	BOOL PreTranslateMessage(MSG* pMsg);
-	void updateScrollInfo(int nPos);
+    BOOL PreTranslateMessage(MSG* pMsg);
+    void updateScrollInfo(int nPos);
 protected:
     CWnd* m_scrollWindow; //real parent is window we overlay the SB
     DECLARE_MESSAGE_MAP()
