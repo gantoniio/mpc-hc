689da8aef22db47e5ba10dd950d45e2680790b12
adipose <3324395+adipose@users.noreply.github.com>
2019-12-29 13:02:11 -0800

    force erase of childview

--- src/mpc-hc/MainFrm.cpp
+++ src/mpc-hc/MainFrm.cpp
@@ -9695,7 +9695,7 @@ void CMainFrame::ToggleFullscreen(bool fToNearest, bool fSwitchScreenResWhenHasT
     m_OSD.HideMessage(false);
 
     if (IsWindows8OrGreater()) {//DWMWA_CLOAK not supported on 7
-        RedrawWindow();
+        m_wndView.RedrawWindow(nullptr, nullptr, RDW_ERASE | RDW_INVALIDATE);
         BOOL setDisabled = FALSE;
         ::DwmSetWindowAttribute(m_hWnd, DWMWA_CLOAK, &setDisabled, sizeof(setDisabled));
     }
