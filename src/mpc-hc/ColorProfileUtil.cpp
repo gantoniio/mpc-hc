258296e1b24deee72625ac2077a5fc3a2e54bea7
adipose <3324395+adipose@users.noreply.github.com>
2020-01-19 10:27:07 -0800

    Remove unused code

--- src/mpc-hc/ColorProfileUtil.cpp
+++ src/mpc-hc/ColorProfileUtil.cpp
@@ -4,10 +4,6 @@
 #include "MainFrm.h"
 #include "Icm.h"
 
-HANDLE ColorProfileUtil::listenThreadHandle = 0;
-HANDLE ColorProfileUtil::quitEvent = 0;
-CWnd* ColorProfileUtil::mainFrame = nullptr;
-
 TCHAR* ColorProfileUtil::getIccProfilePath(HWND wnd) {
 	TCHAR* iccProfilePath = 0;
 
@@ -26,28 +22,6 @@ TCHAR* ColorProfileUtil::getIccProfilePath(HWND wnd) {
 			delete[] iccProfilePath;
 			iccProfilePath = 0;
 		}
-		//hack for realtime changes--GetICMProfile requires process restart, even though it accesses reg keys again (verified with procmon)
-		for (int hc = 0; hc < 2; hc++) {
-			DWORD dwKeySize, dwKeyType;
-			TCHAR iccFile[1024];
-			dwKeySize = sizeof(iccFile);
-			LONG lResult = RegGetValue(hc == 0 ? HKEY_CURRENT_USER : HKEY_LOCAL_MACHINE, REGISTERED_PROFILES_KEY, REGISTERED_PROFILES_SRGB,
-				RRF_RT_REG_SZ, &dwKeyType, (PVOID)&iccFile, &dwKeySize);
-			if (lResult == ERROR_SUCCESS) {
-				TCHAR* tmpProfilePath = DEBUG_NEW TCHAR[icmProfilePathSize + dwKeySize];
-				tmpProfilePath[0] = 0;
-				_tcscat_s(tmpProfilePath, icmProfilePathSize + dwKeySize, iccProfilePath);
-				TCHAR* lastSlash = _tcsrchr(tmpProfilePath, _T('\\'));
-				if (lastSlash) {
-					*(lastSlash + 1) = 0; //terminate after slash
-				}
-				_tcscat_s(tmpProfilePath, icmProfilePathSize + dwKeySize, iccFile);
-				delete[] iccProfilePath;
-				iccProfilePath = tmpProfilePath;
-				break; //quit if we found a match in HKCU, otherwise loop to check for HKLM
-			}
-		}
-
 		DeleteDC(hDC);
 	}
 	return iccProfilePath;
@@ -90,79 +64,4 @@ bool ColorProfileUtil::applyColorProfile(HWND wnd, CImage &image) {
     cmsCloseProfile(hOutputProfile);
 
 	return true;
-}
-
-DWORD WINAPI ColorProfileUtil::ListenThread(LPVOID lpParam) {
-	SetThreadName(DWORD(-1), "ColorProfileUtil::ListenThread");
-
-	if (!mainFrame) return 1;
-
-	DWORD  dwFilter = REG_NOTIFY_CHANGE_NAME | REG_NOTIFY_CHANGE_ATTRIBUTES | REG_NOTIFY_CHANGE_LAST_SET | REG_NOTIFY_CHANGE_SECURITY;
-
-	HANDLE hEvents[3];
-	HKEY   hKeys[3];
-	LONG   lErrorCode;
-
-
-	for (int hc = 0; hc < 2; hc++) {
-		RegOpenKeyEx(hc == 0 ? HKEY_LOCAL_MACHINE : HKEY_CURRENT_USER, REGISTERED_PROFILES_KEY, 0, KEY_READ, &hKeys[hc]);
-		hEvents[hc] = CreateEvent(nullptr, TRUE, FALSE, nullptr);
-		if (hEvents[hc] == nullptr) {
-			TRACE(_T("ColorProfileUtil::ListenThread could not create event\n"));
-			return 1;
-		}
-
-		lErrorCode = RegNotifyChangeKeyValue(hKeys[hc], TRUE, dwFilter, hEvents[hc], TRUE);
-
-		if (lErrorCode != ERROR_SUCCESS) {
-			TRACE(_T("ColorProfileUtil::ListenThread failed in RegNotifyChangeKeyValue\n"));
-			return 1;
-		}
-	}
-
-	hEvents[2] = quitEvent;
-
-	while (true) {
-		DWORD dwObject = WaitForMultipleObjects(3, hEvents, false, INFINITE);
-		if (dwObject == WAIT_FAILED) {
-			TRACE(_T("ColorProfileUtil::ListenThread failed in RegNotifyChangeKeyValue\n"));
-			return 1;
-		} else if (dwObject == WAIT_OBJECT_0+2) {
-			break;
-		} else if (dwObject == WAIT_OBJECT_0 || dwObject == WAIT_OBJECT_0 + 1) {
-			PostMessage(mainFrame->GetSafeHwnd(), WM_COLOR_PROFILE_CHANGED, 0, 0);
-			RegNotifyChangeKeyValue(hKeys[dwObject - WAIT_OBJECT_0], TRUE, dwFilter, hEvents[dwObject - WAIT_OBJECT_0], TRUE);
-		}
-	}
-
-	for (int hc = 0; hc < 2; hc++) {
-		lErrorCode = RegCloseKey(hKeys[hc]);
-		if (lErrorCode != ERROR_SUCCESS) {
-			TRACE(_T("ColorProfileUtil::ListenThread failed to close key\n"));
-		}
-
-		if (!CloseHandle(hEvents[hc])) {
-			TRACE(_T("ColorProfileUtil::ListenThread failed to close handle\n"));
-		}
-	}
-
-	return 0;
-}
-
-
-void ColorProfileUtil::listenForProfileChange(CWnd* _mainFrame) {
-	DWORD threadId;
-	ColorProfileUtil::mainFrame = _mainFrame;
-	quitEvent = CreateEvent(nullptr, TRUE, FALSE, nullptr);
-	listenThreadHandle = ::CreateThread(nullptr, 0, ListenThread, 0, 0, &threadId);
-}
-
-void ColorProfileUtil::stopListeningForProfileChange() {
-	if (quitEvent) {
-		SetEvent(quitEvent);
-	}
-	if (listenThreadHandle && WaitForSingleObject(listenThreadHandle, 10000) == WAIT_TIMEOUT) {
-		ASSERT(FALSE);
-		TerminateThread(listenThreadHandle, 0xDEAD);
-	}
 }
