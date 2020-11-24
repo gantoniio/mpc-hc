eabd160a98b0a363819d43afb26bcd97da8b71e5
lifegpc <g1710431395@gmail.com>
2020-11-24 08:43:40 +0800

    Handle LoadSubtitle and Setsubtitle to get selected Subtitle.

--- src/mpc-hc/MainFrm.cpp
+++ src/mpc-hc/MainFrm.cpp
@@ -15077,7 +15077,9 @@ HRESULT CMainFrame::InsertTextPassThruFilter(IBaseFilter* pBF, IPin* pPin, IPin*
 bool CMainFrame::LoadSubtitle(CString fn, SubtitleInput* pSubInput /*= nullptr*/, bool bAutoLoad /*= false*/)
 {
     CAppSettings& s = AfxGetAppSettings();
-    CComQIPtr<ISubStream> pSubStream;
+    CComQIPtr<ISubStream> pSubStream; 
+    auto& MRU = s.MRU;
+    auto* m_pli = m_wndPlaylistBar.GetCur();
 
     if (!s.IsISRAutoLoadEnabled() && (FindFilter(CLSID_VSFilter, m_pGB) || FindFilter(CLSID_XySubFilter, m_pGB))) {
         // Prevent ISR from loading if VSFilter is already in graph.
@@ -15093,6 +15095,38 @@ bool CMainFrame::LoadSubtitle(CString fn, SubtitleInput* pSubInput /*= nullptr*/
         // This will load all embedded subtitle tracks when user triggers ISR (load external subtitle file) for the first time.
         AddTextPassThruFilter();
     }
+
+    MRU.ReadList();
+    RecentFileEntry r;
+    if (m_pli->m_bYoutubeDL && !m_pli->m_ydlSourceURL.IsEmpty()) {
+        r.fns.Add(m_pli->m_ydlSourceURL);
+    }
+    else {
+        POSITION p = m_pli->m_fns.GetHeadPosition();
+        bool b(true);
+        do {
+            if (p == m_pli->m_fns.GetTailPosition()) b = false;
+            CString t(m_pli->m_fns.GetNext(p));
+            r.fns.Add(t);
+        } while (b);
+        r.fns.FreeExtra();
+    }
+    if (!m_pli->m_label.IsEmpty()) r.title = m_pli->m_label;
+    bool found = false;
+    if (m_pli->m_subs.GetCount() > 0) {
+        POSITION p = m_pli->m_subs.GetHeadPosition();
+        bool b(true);
+        do {
+            if (p == m_pli->m_subs.GetTailPosition()) b = false;
+            CString t(m_pli->m_subs.GetNext(p));
+            if (t == fn) found = true;
+            r.subs.Add(t);
+        } while (b);
+        r.subs.FreeExtra();
+    }
+    if (!found) r.subs.Add(fn);
+    MRU.Add(r);
+    MRU.WriteList();
 
     CString videoName;
     if (GetPlaybackMode() == PM_FILE) {
@@ -15161,6 +15195,8 @@ bool CMainFrame::SetSubtitle(int i, bool bIsOffset /*= false*/, bool bDisplayMes
 
     SubtitleInput* pSubInput = GetSubtitleInput(i, bIsOffset);
     bool success = false;
+    auto& MRU = AfxGetAppSettings().MRU;
+    auto* m_pli = m_wndPlaylistBar.GetCur();
 
     if (pSubInput) {
         CComHeapPtr<WCHAR> pName;
@@ -15189,6 +15225,7 @@ bool CMainFrame::SetSubtitle(int i, bool bIsOffset /*= false*/, bool bDisplayMes
         }
         SetSubtitle(*pSubInput, true);
 
+        CString path;
         if (!pName) {
             LCID lcid = 0;
             pSubInput->pSubStream->GetStreamInfo(0, &pName, &lcid);
@@ -15197,7 +15234,40 @@ bool CMainFrame::SetSubtitle(int i, bool bIsOffset /*= false*/, bool bDisplayMes
             } else {
                 currentSubLang.Empty();
             }
+            path = pSubInput->pSubStream->GetPath();
         }
+
+        MRU.ReadList();
+        RecentFileEntry r;
+        if (m_pli->m_bYoutubeDL && !m_pli->m_ydlSourceURL.IsEmpty()) {
+            r.fns.Add(m_pli->m_ydlSourceURL);
+        }
+        else {
+            POSITION p = m_pli->m_fns.GetHeadPosition();
+            bool b(true);
+            do {
+                if (p == m_pli->m_fns.GetTailPosition()) b = false;
+                CString t(m_pli->m_fns.GetNext(p));
+                r.fns.Add(t);
+            } while (b);
+            r.fns.FreeExtra();
+        }
+        if (!m_pli->m_label.IsEmpty()) r.title = m_pli->m_label;
+        bool found = false;
+        if (m_pli->m_subs.GetCount() > 0) {
+            POSITION p = m_pli->m_subs.GetHeadPosition();
+            bool b(true);
+            do {
+                if (p == m_pli->m_subs.GetTailPosition()) b = false;
+                CString t(m_pli->m_subs.GetNext(p));
+                if (t == path) found = true;
+                r.subs.Add(t);
+            } while (b);
+            r.subs.FreeExtra();
+        }
+        if (!found && !path.IsEmpty()) r.subs.Add(path);
+        MRU.Add(r);
+        MRU.WriteList();
 
         if (bDisplayMessage && pName) {
             m_OSD.DisplayMessage(OSD_TOPLEFT, GetStreamOSDString(CString(pName), LCID(-1), 2));
