8cf37243f176e9b5cd5ee7ad1a9db5dd8ec365fc
adipose <3324395+adipose@users.noreply.github.com>
2024-02-25 12:51:55 -0800

    .

--- src/mpc-hc/SubtitleDlDlg.cpp
+++ src/mpc-hc/SubtitleDlDlg.cpp
@@ -262,8 +262,8 @@ BOOL CSubtitleDlDlg::OnInitDialog()
 
 BOOL CSubtitleDlDlg::PreTranslateMessage(MSG* pMsg)
 {
-    // Inhibit default handling for the Enter key when the list has the focus and an item is selected.
     if (pMsg->message == WM_KEYDOWN && pMsg->wParam == VK_RETURN) {
+        // Inhibit default handling for the Enter key when the list has the focus and an item is selected.
         if (pMsg->hwnd == m_list.GetSafeHwnd() && m_list.GetSelectedCount() > 0) {
             return FALSE;
         }
