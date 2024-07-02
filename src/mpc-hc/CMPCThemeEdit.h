4e8056dcee89225c4d2d37fc955f505bdaeeabcb
adipose <3324395+adipose@users.noreply.github.com>
2024-07-02 11:35:25 -0700

    remove unused OnEraseBkgnd

--- src/mpc-hc/CMPCThemeEdit.h
+++ src/mpc-hc/CMPCThemeEdit.h
@@ -28,7 +28,6 @@ protected:
     afx_msg BOOL OnMouseWheel(UINT nFlags, short zDelta, CPoint pt);
     afx_msg void OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
     afx_msg void OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
-    afx_msg BOOL OnEraseBkgnd(CDC* pDC);
 public:
     afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
 };
