dced2cd437cc42037ebc2a64acbf8a7621c69f98
clsid2 <4704996+clsid2@users.noreply.github.com>
2025-06-03 23:09:53 +0200

    .

--- src/mpc-hc/MainFrm.cpp
+++ src/mpc-hc/MainFrm.cpp
@@ -3260,6 +3260,7 @@ LRESULT CMainFrame::OnGraphNotify(WPARAM wParam, LPARAM lParam)
                 OnVideoSizeChanged(bWasAudioOnly);
                 m_statusbarVideoSize.Format(_T("%dx%d"), size.cx, size.cy);
                 UpdateDXVAStatus();
+                CheckSelectedVideoStream();
             }
             break;
             case EC_LENGTH_CHANGED: {
