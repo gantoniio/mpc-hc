af688c075c9fe1c3b42928544ae3a07b44a8ce0c
clsid2 <4704996+clsid2@users.noreply.github.com>
2025-07-17 14:27:09 +0200

    .

--- src/mpc-hc/mplayerc.cpp
+++ src/mpc-hc/mplayerc.cpp
@@ -2451,7 +2451,7 @@ void CMPlayerCApp::OnAppAbout()
 void CMPlayerCApp::SetClosingState()
 {
     m_fClosingState = true;
-#if USE_DRDUMP_CRASH_REPORTER & (MPC_VERSION_REV < 10)
+#if USE_DRDUMP_CRASH_REPORTER & ((MPC_VERSION_REV < 10) || MPC_VERSION_PATCH == 1)
     DisableCrashReporter();
 #endif
 }
