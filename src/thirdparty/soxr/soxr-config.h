194ab9986534df72918aa548bc14da68636cd535
clsid2 <4704996+clsid2@users.noreply.github.com>
2025-08-04 20:46:25 +0200

    update libsoxr

--- src/thirdparty/soxr/soxr-config.h
+++ src/thirdparty/soxr/soxr-config.h
@@ -1,13 +1,29 @@
-#pragma once
+#if !defined soxr_config_included
+#define soxr_config_included
 
-#define HAVE_SINGLE_PRECISION 1
-#define HAVE_DOUBLE_PRECISION 1
-#define HAVE_SIMD 1
+#define AVCODEC_FOUND 0
+#define AVUTIL_FOUND 0
+#define WITH_PFFFT 1
+
 #define HAVE_FENV_H 1
+#define HAVE_STDBOOL_H 1
+#define HAVE_STDINT_H 1
 #define HAVE_LRINT 1
+#define HAVE_BIGENDIAN 0
 
-#define HAVE_AVFFT 0
-#define WORDS_BIGENDIAN 0
+#define WITH_CR32 1
+#define WITH_CR32S 1
+#define WITH_CR64 1
+#define WITH_CR64S 1
+#define WITH_VR32 1
 
-#include <inttypes.h>
-#include <stdbool.h>
+#define WITH_HI_PREC_CLOCK 1
+#define WITH_FLOAT_STD_PREC_CLOCK 0
+
+#ifdef _DEBUG
+#define WITH_DEV_TRACE 1
+#else
+#define WITH_DEV_TRACE 0
+#endif
+
+#endif
